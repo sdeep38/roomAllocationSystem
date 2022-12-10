f36aa93ebcd8421d3366ab712f268043037d09c0
Soumyadeep <soumyadip243@gmail.com>
2022-12-11 00:28:45 +0530

    added fetchUser function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import React, { useContext, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import Navbar from '../components/Navbar'
+import Header from '../components/Header'
 import ScrollMenu from '../components/ScrollMenu'
 import { AuthContext } from '../context/authContext'
 import '../styles/Home.css'
@@ -52,10 +52,7 @@ export default function Home() {
         },
     ]
 
-    const handleOptionChange = (e) => {
-        console.log('User Selected value : ', e.target.value)
-    }
-
+    //get all students data
     const [students, setStudents] = useState([])
 
     useEffect(() => {
@@ -70,35 +67,35 @@ export default function Home() {
         fetchData();
     }, [])
 
+    //get single student data
+    const [userData, setUserData] = useState([])
+
+    const fetchUser = async (userId) => {
+        try {
+            const res = await axios.get(`/users/getUser/${userId}`)
+            setUserData(res.data)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
     return (
         <>
 
-            <Navbar />
-            <div className="page-heading">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-12">
-                            <div className="ui-text">
-                                <h1>Welcome <span style={{ textTransform: "uppercase" }}>{currentUser?.name}</span></h1>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
+            <Header currentUser = {currentUser}/>
             <ScrollMenu />
 
             <div className="main-content">
                 <div className="container pt-5">
                     <div className="row">
-                        <div className="col-md-8 col-sm-9">
+                        <div className="col-lg-8 col-md-8 col-sm-12">
                             <div className="room-view-wrapper">
                                 <h2>Room Details</h2>
                                 <div className="filter-form">
                                     <form className="row row-cols-lg-auto g-3 align-items-center">
                                         <div className="col-lg-4">
                                             <label className="visually-hidden" htmlFor="inlineFormSelectPref">Preference</label>
-                                            <select className="form-select" id="inlineFormSelectPref" onChange={handleOptionChange}>
+                                            <select className="form-select" id="inlineFormSelectPref">
                                                 <option>Select Floor</option>
                                                 {floorOptions.map((option, index) => {
                                                     return <option key={index} value={option.value}>{option.label}</option>
@@ -108,7 +105,7 @@ export default function Home() {
 
                                         <div className="col-lg-4">
                                             <label className="visually-hidden" htmlFor="inlineFormSelectPref">Preference</label>
-                                            <select className="form-select" id="inlineFormSelectPref" onChange={handleOptionChange}>
+                                            <select className="form-select" id="inlineFormSelectPref">
                                                 <option>Select Block</option>
                                                 {blockOptions.map((option, index) => {
                                                     return <option key={index} value={option.value}>{option.label}</option>
@@ -138,7 +135,7 @@ export default function Home() {
                                                     <th scope="row">{student.id}</th>
                                                     <td>{student.block} - {student.room}</td>
                                                     <td>{student.name}</td>
-                                                    <td><Link>View</Link></td>
+                                                    <td><Link onClick={() => {fetchUser(student.id)}}>View</Link></td>
                                                 </tr>
                                             })}
 
@@ -147,15 +144,15 @@ export default function Home() {
                                 </div>
                             </div>
                         </div>
-                        <div className="col-md-4 col-sm-3">
+                        <div className="col-lg-4 col-md-4 col-sm-12">
                             <div className="block-right">
                                 <h4>Student Details</h4>
                                 <ul className="list-group list-group-flush">
-                                    <li className="list-group-item">Name<Link style={{ float: "right" }}>{currentUser.name}</Link></li>
-                                    <li className="list-group-item">Roll No<Link style={{ float: "right" }}>{currentUser.roll}</Link></li>
-                                    <li className="list-group-item">Email id<Link style={{ float: "right" }}>{currentUser.email}</Link></li>
-                                    <li className="list-group-item">Contact No<Link style={{ float: "right" }}>{currentUser.phone}</Link></li>
-                                    <li className="list-group-item">Room No<Link style={{ float: "right" }}>{currentUser.block} - {currentUser.room}</Link></li>
+                                    <li className="list-group-item">Name<Link style={{ float: "right" }}>{userData[0]?.name}</Link></li>
+                                    <li className="list-group-item">Roll No<Link style={{ float: "right" }}>{userData[0]?.roll}</Link></li>
+                                    <li className="list-group-item">Email id<Link style={{ float: "right" }}>{userData[0]?.email}</Link></li>
+                                    <li className="list-group-item">Contact No<Link style={{ float: "right" }}>{userData[0]?.phone}</Link></li>
+                                    <li className="list-group-item">Room No<Link style={{ float: "right" }}>{userData[0]?.block} - {userData[0]?.room}</Link></li>
                                 </ul>
                             </div>
                         </div>
