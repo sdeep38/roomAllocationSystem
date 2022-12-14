0c01f67ad584343b0cdcc5280af5167dcc43514c
Soumyadeep <soumyadip243@gmail.com>
2022-12-14 21:24:37 +0530

    added update contact num feature

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,81 +4,120 @@ import profileImage from '../images/profile.jpg'
 import Header from '../components/Header'
 import { AuthContext } from '../context/authContext'
 import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { useEffect } from 'react'
+import { useRef } from 'react'
 
 export default function Profile() {
 
     const { currentUser } = useContext(AuthContext)
 
-    const [formInput, setFormInput] = useState(`${currentUser?.phone}`)
 
-    const [inputs, setInputs] = useState({contact: ""})
+    const [userData, setUserData] = useState({
+        name: '',
+        roll: '',
+        phone: '',
+        block: '',
+        email: '',
+        room: '',
+    })
 
-    const navigate = useNavigate()
+    const [newContact, setNewContact] = useState(null)
 
-    const enableEdit = (e) => {
-        console.log(e.target.value)
+    const [resStatus, setResStatus] = useState(null)
+
+    const [enableUpdate, setEnableUpdate] = useState(true)
+
+    const contactInput = useRef(null);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        if(newContact == null){
+            alert('No changes detected')
+        }
+        else{
+            try {
+                const res = await axios.put(`/users/updateUser/${currentUser.id}`, { contact: newContact })
+                setResStatus(res.data.stat)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        
     }
 
-    const handleChange = (e) => {
-        setFormInput(e.target.value)
-        setInputs(next => ({ ...next, [e.target.name]: e.target.value }))
-        console.log(inputs)
-    }
-
-    const handleSubmit = () => {
-
-    }
-
+    useEffect(() => {
+        const fetchCurrentUserData = async () => {
+            try {
+                const res = await axios.get(`/users/getUser/${currentUser.id}`)
+                setUserData(res.data[0])
+            } catch (err) {
+                console.log(err.response)
+            }
+        }
+        fetchCurrentUserData();
+    })
 
     return (
         <>
             <Header currentUser={currentUser} />
             <div className="container spacet50">
                 <div className="row">
+
+                    {/* profile-left */}
                     <div className="col-lg-4">
                         <div className="profile profile-left">
                             <div className="profile-img-top">
                                 <img src={profileImage} alt="userProfile" />
-                                <h4 className="username">{currentUser?.name}</h4>
+                                <h4 className="username">{userData.name}</h4>
                                 <span className="user-role">Student</span>
                             </div>
 
-                            <ul>
+                            {/* <ul>
                                 <li><a href="/">Change Password</a></li>
                                 <li><a href="/">Logout</a></li>
-                            </ul>
+                            </ul> */}
                         </div>
                     </div>
+
+                    {/* profile-right */}
                     <div className="col-lg-8">
                         <div className="profile profile-right p-0">
                             <div className="heading">My Profile</div>
+
+                            {resStatus && <div className="alert alert-success d-flex align-items-center mt-3 mb-0 alert-dismissible fade show" role="alert">
+                                <div className='update-status'>
+                                    {resStatus}
+                                </div>
+                                <span className="fa fa-xmark btn-close" type="button" data-bs-dismiss="alert" aria-label="Close"></span>
+                            </div>}
                             <div className="details">
 
-                                <div className="row g-3">
+                                <form className="row g-3">
                                     <div className="user-data-wrap">
-                                        <label htmlFor="inputEmail4" className="form-label">Email</label>
-                                        <input type="email" className="form-input" id="inputEmail4" value={currentUser?.email} readOnly={true} />
+                                        <label htmlFor="inputEmail" className="form-label">Email Id</label>
+                                        <input type="email" className="form-input w-70" name='email' id="inputEmail" value={userData.email} readOnly={true} />
                                     </div>
                                     <div className="user-data-wrap">
-                                        <label htmlFor="inputPassword4" className="form-label">Roll No</label>
-                                        <input type="text" className="form-input" id="inputPassword4" value={currentUser?.roll} readOnly={true} />
+                                        <label htmlFor="inputRoll" className="form-label">Roll No</label>
+                                        <input type="text" className="form-input w-70" name="roll" id="inputRoll" value={userData.roll} readOnly={true} />
                                     </div>
                                     <div className="user-data-wrap">
-                                        <label htmlFor="inputCity" className="form-label">Contact Number</label>
-                                        <input type="text" className="form-input" id="inputCity" name="contact" value={formInput} onChange={handleChange} maxLength="10" />
-                                        <span className="fa fa-pencil edit-btn"></span>
+                                        <label htmlFor="inputPhone" className="form-label">Contact Number</label>
+                                        <input type="text" ref={contactInput} className="form-input w-70" id="inputPhone" name="phone" value={newContact || userData.phone} onChange={(e) => { setNewContact(e.target.value); console.log(newContact) }} maxLength="10" readOnly={enableUpdate}/>
+                                        <span className="edit-btn" onClick={(state) => {setEnableUpdate(!state); contactInput.current.focus()}}>Edit</span>
                                     </div>
                                     <div className="user-data-wrap">
-                                        <label htmlFor="inputZip" className="form-label">Room Number</label>
-                                        <input type="text" className="form-input" id="room_no" value={currentUser?.block + " - " + currentUser?.room} readOnly={true} />
+                                        <label htmlFor="inputRoom" className="form-label">Room Number</label>
+                                        <input type="text" className="form-input w-70" name='room' id="inputRoom" value={userData.block + " - " + userData.room} readOnly={true} />
                                     </div>
 
                                     <div className="col-12">
-                                        <button className='btn btn-secondary btn-sm' onClick={() => navigate(-1)}>Back to Dashboard</button>
-                                        <button className='btn btn-secondary btn-sm' onClick={handleSubmit}>Done</button>
+                                        {/* <button className='btn btn-secondary btn-sm' onClick={() => navigate(-1)}>Back to Dashboard</button> */}
+                                        <button type="submit" className='btn btn-primary btn-sm' onClick={handleSubmit}>Update</button>
                                     </div>
 
-                                </div>
+                                </form>
                             </div>
                         </div>
                     </div>
