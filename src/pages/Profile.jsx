0009cb39b8b30efa97604978f1b70226c8729ed4
Soumyadeep <soumyadip243@gmail.com>
2022-12-20 21:33:39 +0530

    modified few elements

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -32,24 +32,26 @@ export default function Profile() {
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-        if(newContact == null){
+        if (newContact == null) {
             alert('No changes detected')
         }
-        else{
+        else {
             try {
-                const res = await axios.put(`/users/updateUser/${currentUser.id}`, { contact: newContact })
+                const res = await axios.put(`/users/updateUser/${currentUser?.id}`, { contact: newContact })
                 setResStatus(res.data.stat)
             } catch (error) {
                 console.log(error)
             }
         }
-        
+
     }
+
+    const userType = currentUser?.auth
 
     useEffect(() => {
         const fetchCurrentUserData = async () => {
             try {
-                const res = await axios.get(`/users/getUser/${currentUser.id}`)
+                const res = await axios.get(`/users/getUser/${currentUser?.resultSet.id}`)
                 setUserData(res.data[0])
             } catch (err) {
                 console.log(err.response)
@@ -58,6 +60,11 @@ export default function Profile() {
         fetchCurrentUserData();
     })
 
+    const Position = {
+        gSec: "General Secretary",
+        hallPresident: "Hall President",
+        Warden: "Warden",
+    }
     return (
         <>
             <Header currentUser={currentUser} />
@@ -69,8 +76,8 @@ export default function Profile() {
                         <div className="profile profile-left">
                             <div className="profile-img-top">
                                 <img src={profileImage} alt="userProfile" />
-                                <h4 className="username">{userData.name}</h4>
-                                <span className="user-role">Student</span>
+                                <h4 className="username">{currentUser?.resultSet.name}</h4>
+                                <span className="user-role">{userType}</span>
                             </div>
 
                             {/* <ul>
@@ -96,24 +103,32 @@ export default function Profile() {
                                 <form className="row g-3">
                                     <div className="user-data-wrap">
                                         <label htmlFor="inputEmail" className="form-label">Email Id</label>
-                                        <input type="email" className="form-input w-70" name='email' id="inputEmail" value={userData.email} readOnly={true} />
-                                    </div>
-                                    <div className="user-data-wrap">
-                                        <label htmlFor="inputRoll" className="form-label">Roll No</label>
-                                        <input type="text" className="form-input w-70" name="roll" id="inputRoll" value={userData.roll} readOnly={true} />
+                                        <input type="email" className="form-input w-70" name='email' id="inputEmail" value={userData?.email} readOnly={true} />
                                     </div>
                                     <div className="user-data-wrap">
                                         <label htmlFor="inputPhone" className="form-label">Contact Number</label>
-                                        <input type="text" ref={contactInput} className="form-input w-70" id="inputPhone" name="phone" value={newContact || userData.phone} onChange={(e) => { setNewContact(e.target.value); console.log(newContact) }} maxLength="10" readOnly={enableUpdate}/>
-                                        <span className="edit-btn" onClick={(state) => {setEnableUpdate(!state); contactInput.current.focus()}}>Edit</span>
-                                    </div>
-                                    <div className="user-data-wrap">
-                                        <label htmlFor="inputRoom" className="form-label">Room Number</label>
-                                        <input type="text" className="form-input w-70" name='room' id="inputRoom" value={userData.block + " - " + userData.room} readOnly={true} />
+                                        <input type="text" ref={contactInput} className="form-input w-70" id="inputPhone" name="phone" value={newContact || userData?.phone} onChange={(e) => { setNewContact(e.target.value); console.log(newContact) }} maxLength="10" readOnly={enableUpdate} />
+                                        <span className="edit-btn" onClick={(state) => { setEnableUpdate(!state); contactInput.current.focus() }}>Edit</span>
                                     </div>
 
+                                    {currentUser.auth === 'student' ?
+                                        [<div className="user-data-wrap" key={currentUser?.resultSet.id + 1}>
+                                            <label htmlFor="inputRoll" className="form-label">Roll No</label>
+                                            <input type="text" className="form-input w-70" name="roll" id="inputRoll" value={userData?.roll} readOnly={true} />
+                                        </div>,
+
+                                        <div className="user-data-wrap" key={currentUser?.resultSet.id + 2}>
+                                            <label htmlFor="inputRoom" className="form-label">Room Number</label>
+                                            <input type="text" className="form-input w-70" name='room' id="inputRoom" value={userData?.block + " - " + userData?.room} readOnly={true} />
+                                        </div>] :
+
+                                        [<div className="user-data-wrap" key={currentUser?.resultSet.id + 3}>
+                                            <label htmlFor="inputPosition" className="form-label">Position</label>
+                                            <input type="text" className="form-input w-70" name="roll" id="inputPosition" value={Position[userData?.position]} readOnly={true} />
+                                        </div>]
+                                    }
+    
                                     <div className="col-12">
-                                        {/* <button className='btn btn-secondary btn-sm' onClick={() => navigate(-1)}>Back to Dashboard</button> */}
                                         <button type="submit" className='btn btn-primary btn-sm' onClick={handleSubmit}>Update</button>
                                     </div>
 
