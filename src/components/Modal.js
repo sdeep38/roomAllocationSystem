e7092e6041027c14022cec503f3c9589b0fa790f
Soumyadeep <soumyadip243@gmail.com>
2022-12-20 21:35:34 +0530

    added new file

---
+++ src/components/Modal.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useState } from 'react'
+
+export default function Modal({showModal}) {
+
+    return (
+        <>
+
+            <div className={`modal fade${showModal ? ' show':''}`} tabIndex="-1" id='exampleModal' role='dialog'>
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Modal title</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <p>Modal body text goes here.</p>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary">Close</button>
+                            <button type="button" className="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
