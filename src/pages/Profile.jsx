bd8f1179b26b20d4717769eefd3d23c5ed2646a9
Soumyadeep <soumyadip243@gmail.com>
2023-06-13 00:40:58 +0530

    added form validation, created new form

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,71 +3,127 @@ import '../styles/Profile.css'
 import profileImage from '../images/profile.jpg'
 import Header from '../components/Header'
 import { AuthContext } from '../context/authContext'
-import { useNavigate } from 'react-router-dom'
+import { Navigate, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import { useEffect } from 'react'
 import { useRef } from 'react'
 
 export default function Profile() {
 
-    const { currentUser } = useContext(AuthContext)
-
-
-    const [userData, setUserData] = useState({
-        name: '',
-        roll: '',
-        phone: '',
-        block: '',
-        email: '',
-        room: '',
-    })
-
-    const [newContact, setNewContact] = useState(null)
-
-    const [resStatus, setResStatus] = useState(null)
-
-    const [enableUpdate, setEnableUpdate] = useState(true)
-
+    const index = 8
+    
+    const { logout, currentUser } = useContext(AuthContext)
+    
+    const navigate = useNavigate()
     const contactInput = useRef(null);
+    
+    const [userData, setUserData] = useState(null)
+    const [newContact, setNewContact] = useState(null)
+    const [enableUpdate, setEnableUpdate] = useState(false)
+    const [resStatus, setResStatus] = useState(null)
+    
+    const [activeTab, setActiveTab] = useState('tab1')
+    const userType = currentUser?.authorizedAs
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        if (newContact == null) {
-            alert('No changes detected')
-        }
-        else {
-            try {
-                const res = await axios.put(`/users/updateUser/${currentUser?.id}`, { contact: newContact })
-                setResStatus(res.data.stat)
-            } catch (error) {
-                console.log(error)
-            }
+    const [inputs, setInputs] = useState({
+        password1: "",
+        password2: "",
+        password0: "",
+        userId: "",
+    })
+    
+    const handleChange = (e) => {
+        setInputs(prev => ({ ...prev, [e.target.name]: e.target.value }))
+    }
+
+    const handleLogout = async () => {
+        await logout()
+        navigate('/login')
+    }
+
+    const handleSubmit = async () => {
+        try {
+            const res = await axios.put('/users/updateUser/', { contact: newContact })
+            console.log(res.data)
+            setResStatus(res.data)
+        } catch (error) {
+            console.log(error)
         }
 
     }
 
-    const userType = currentUser?.auth
+    const handlePasswordChangeForm = async () => {
+        try {
+            const res = await axios.post('/auth/changePassword', inputs)
+            console.log(res.data)
+            if(res.data.status === 'success'){
+                setResStatus(res.data.message)
+            }
+            else if(res.data.status === 'error') {
+                alert(res.data.message)
+            }
+        } catch (error) {
+            alert(error.response.data)
+        }
+
+    }
+
+    const validateForm = (e) => {
+        e.preventDefault()
+
+        if (newContact === null || newContact === userData?.phone) {
+            alert('No changes detected')
+        }
+        else {
+            const choice = window.confirm('Are you sure you want to make the changes ?')
+            choice === true ? handleSubmit() : setEnableUpdate(false)
+        }
+    }
+
+    const validatePasswordForm = (e) => {
+        e.preventDefault()
+        if (inputs.password0 === '' || inputs.password1 === '' || inputs.password2 === '') {
+            alert("All fields are required")
+        }
+        else if (inputs.password0 === inputs.password1) {
+            alert("New Password should be different from old password")
+        }
+        else if (inputs.password1 === inputs.password2) {
+            setInputs({ userId: userData?.roll })
+            console.log(inputs)
+            // handlePasswordChangeForm()
+
+        }
+        else {
+            alert("ERROR : New Passwords don't match")
+        }
+    }
+
 
     useEffect(() => {
         const fetchCurrentUserData = async () => {
             try {
-                const res = await axios.get(`/users/getUser/${currentUser?.resultSet.id}`)
-                setUserData(res.data[0])
+                const res = await axios.get('/users/getUser/')
+                console.log(res.data)
+                setUserData(res.data.dataSet)
+                // setUserDataStatus(true)
             } catch (err) {
-                console.log(err.response)
+                console.log(err.response.data)
             }
         }
-        fetchCurrentUserData();
-    })
+        if(!userData) fetchCurrentUserData()
+    }, [])
 
     const Position = {
         gSec: "General Secretary",
         hallPresident: "Hall President",
         Warden: "Warden",
     }
+
+
     return (
         <>
-            <Header currentUser={currentUser} />
+            <Header title='Settings' />
             <div className="container spacet50">
                 <div className="row">
 
@@ -76,64 +132,101 @@ export default function Profile() {
                         <div className="profile profile-left">
                             <div className="profile-img-top">
                                 <img src={profileImage} alt="userProfile" />
-                                <h4 className="username">{currentUser?.resultSet.name}</h4>
+                                <h4 className="username">{currentUser?.username}</h4>
                                 <span className="user-role">{userType}</span>
                             </div>
 
-                            {/* <ul>
-                                <li><a href="/">Change Password</a></li>
-                                <li><a href="/">Logout</a></li>
-                            </ul> */}
+
                         </div>
+                        <div className="group-buttons" role='tablist'>
+                            <a href="#" className={activeTab === 'tab1' ? 'setting-tabs active' : 'setting-tabs'} onClick={() => setActiveTab('tab1')}>Edit Profile</a>
+                            <a href='#' className={activeTab === 'tab2' ? 'setting-tabs active' : 'setting-tabs'} onClick={() => setActiveTab('tab2')}>Change Password</a>
+                        </div>
+
                     </div>
 
                     {/* profile-right */}
                     <div className="col-lg-8">
                         <div className="profile profile-right p-0">
-                            <div className="heading">My Profile</div>
-
-                            {resStatus && <div className="alert alert-success d-flex align-items-center mt-3 mb-0 alert-dismissible fade show" role="alert">
+                            {resStatus && <div className={"alert " + (resStatus.status === 'success' ? 'alert-success' : 'alert-danger') + " d-flex align-items-center mt-3 mb-0 alert-dismissible fade show"} role="alert">
                                 <div className='update-status'>
-                                    {resStatus}
+                                    {resStatus.message}
                                 </div>
                                 <span className="fa fa-xmark btn-close" type="button" data-bs-dismiss="alert" aria-label="Close"></span>
                             </div>}
-                            <div className="details">
 
-                                <form className="row g-3">
-                                    <div className="user-data-wrap">
-                                        <label htmlFor="inputEmail" className="form-label">Email Id</label>
-                                        <input type="email" className="form-input w-70" name='email' id="inputEmail" value={userData?.email} readOnly={true} />
-                                    </div>
-                                    <div className="user-data-wrap">
-                                        <label htmlFor="inputPhone" className="form-label">Contact Number</label>
-                                        <input type="text" ref={contactInput} className="form-input w-70" id="inputPhone" name="phone" value={newContact || userData?.phone} onChange={(e) => { setNewContact(e.target.value); console.log(newContact) }} maxLength="10" readOnly={enableUpdate} />
-                                        <span className="edit-btn" onClick={(state) => { setEnableUpdate(!state); contactInput.current.focus() }}>Edit</span>
+                            {activeTab === 'tab1' ? <div className="tab1">
+                                <div className="heading mb-3">User Information</div>
+
+                                <form className="row g-3 profile-form">
+                                    <div className="col-12 user-data-wrap">
+                                        <label htmlFor="inputName" className="form-label">Name</label>
+                                        <input type="text" className="form-control profile-input" name='name' id="inputName" value={currentUser?.username} disabled={true} />
                                     </div>
 
-                                    {currentUser.auth === 'student' ?
-                                        [<div className="user-data-wrap" key={currentUser?.resultSet.id + 1}>
-                                            <label htmlFor="inputRoll" className="form-label">Roll No</label>
-                                            <input type="text" className="form-input w-70" name="roll" id="inputRoll" value={userData?.roll} readOnly={true} />
+                                    <div className="col-md-12 user-data-wrap">
+                                        <label htmlFor="inputEmail" className="form-label">Email Address</label>
+                                        <input type="email" className="form-control profile-input" name='email' id="inputEmail" value={userData?.email} disabled={true} />
+                                    </div>
+
+                                    {currentUser?.authorizedAs === 'student' ?
+                                        [<div className="col-md-4 user-data-wrap" key={index + 1}>
+                                            <label htmlFor="inputRoll" className="form-label">Roll No.</label>
+                                            <input type="text" className="form-control profile-input" name="roll" id="inputRoll" value={userData?.roll} disabled={true} />
                                         </div>,
 
-                                        <div className="user-data-wrap" key={currentUser?.resultSet.id + 2}>
-                                            <label htmlFor="inputRoom" className="form-label">Room Number</label>
-                                            <input type="text" className="form-input w-70" name='room' id="inputRoom" value={userData?.block + " - " + userData?.room} readOnly={true} />
+                                        <div className="col-md-4 user-data-wrap" key={index + 2}>
+                                            <label htmlFor="inputRoom" className="form-label">Room</label>
+                                            <input type="text" className="form-control profile-input" name='room' id="inputRoom" value={(userData?.block === null) ? 'Not Assigned' : (userData?.block + '-' + userData?.room)} disabled={true} />
                                         </div>] :
 
-                                        [<div className="user-data-wrap" key={currentUser?.resultSet.id + 3}>
+                                        [<div className="col-8 user-data-wrap" key={index + 3}>
                                             <label htmlFor="inputPosition" className="form-label">Position</label>
-                                            <input type="text" className="form-input w-70" name="roll" id="inputPosition" value={Position[userData?.position]} readOnly={true} />
+                                            <input type="text" className="form-control profile-input" name="roll" id="inputPosition" value={Position[userData?.position]} disabled={true} />
                                         </div>]
                                     }
-    
-                                    <div className="col-12">
-                                        <button type="submit" className='btn btn-primary btn-sm' onClick={handleSubmit}>Update</button>
+
+                                    <div className="col-md-4 user-data-wrap relative">
+                                        <label htmlFor="inputPhone" className="form-label">Contact No.</label>
+                                        <input type="tel" ref={contactInput} className="form-control profile-input" id="inputPhone" name="phone" value={newContact || userData?.phone} onChange={(e) => { setNewContact(e.target.value); }} maxLength="10" disabled={!enableUpdate} />
+                                        <span className="edit-btn" onClick={() => { setEnableUpdate(!enableUpdate) }}><i className='fa fa-pencil'></i></span>
+                                    </div>
+
+
+
+                                    <div className="col-12 center">
+                                        <button type="submit" className='btn btn-primary btn-sm' onClick={validateForm} disabled={!enableUpdate}>Update</button>
                                     </div>
 
                                 </form>
-                            </div>
+                            </div> : <div className="tab2">
+                                <div className="heading mb-3">Change Password</div>
+                                <form className='pt-3' id='PasswordChangeForm'>
+                                    <div className="row mb-3">
+                                        <label htmlFor="inputCurrentPassword" className="col-sm-4 form-label">Current Password</label>
+                                        <div className="col-sm-8">
+                                            <input type="password" className="form-control profile-input" id="inputCurrentPassword" name='password0' onChange={handleChange} />
+                                        </div>
+                                    </div>
+                                    <div className="row mb-3">
+                                        <label htmlFor="inputNewPassword1" className="col-sm-4 form-label">New Password</label>
+                                        <div className="col-sm-8">
+                                            <input type="password" className="form-control profile-input" id="inputNewPassword1" name='password1' onChange={handleChange} />
+                                        </div>
+                                    </div>
+                                    <div className="row mb-3">
+                                        <label htmlFor="inputNewPassword2" className="col-sm-4 form-label">Confirm New Password</label>
+                                        <div className="col-sm-8">
+                                            <input type="password" className="form-control profile-input" id="inputNewPassword2" name='password2' onChange={handleChange} />
+                                        </div>
+                                    </div>
+
+                                    <div className="row mb-3 center">
+                                        <button type="submit" className='col-sm-2 btn btn-primary btn-sm mt-3' onClick={validatePasswordForm}>Submit</button>
+                                    </div>
+                                </form>
+                            </div>}
+
                         </div>
                     </div>
                 </div>
