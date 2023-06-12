9f73ddd5ec7a7a785bf1ff76b9ead38926e5e0ae
Soumyadeep <soumyadip243@gmail.com>
2023-06-13 00:45:50 +0530

    updated component

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,25 +1,26 @@
 import React from 'react'
 import { useState } from 'react'
 
-export default function Modal({showModal}) {
+export default function Modal({ showModal, onHide }) {
 
     return (
         <>
 
-            <div className={`modal fade${showModal ? ' show':''}`} tabIndex="-1" id='exampleModal' role='dialog'>
+            <div className={`modal fade${showModal ? ' show open-modal' : ''}`} tabIndex="-1" id='exampleModal' role='dialog'>
                 <div className="modal-dialog modal-dialog-centered">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h5 className="modal-title">Modal title</h5>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            <h5 className="modal-title">E - 417</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={onHide}></button>
                         </div>
                         <div className="modal-body">
-                            <p>Modal body text goes here.</p>
+                            <ul className="list-group list-group-flush">
+                                <li className="list-group-item">Status<span className="badge bg-primary rounded-pill">OCCUPIED</span></li>
+                                <li className="list-group-item">Accomodation<span className="text-primary fw-600">3 sharing</span></li>
+                                <li className="list-group-item">Vacancies<span className="text-danger fw-600">0</span></li>
+                            </ul>
                         </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary">Close</button>
-                            <button type="button" className="btn btn-primary">Save changes</button>
-                        </div>
+
                     </div>
                 </div>
             </div>
