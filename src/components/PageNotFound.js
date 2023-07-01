c025cbe2ca33fafafea6588390b2ab46263176a7
Soumyadeep <soumyadip243@gmail.com>
2023-07-01 22:16:15 +0530

    added new component

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function PageNotFound() {
+  let navigate = useNavigate()
   return (
-    <div>
-      <p className="text-danger">404 : Page Not Found</p>
+    <div className="wrapper">
+    <div className='container'>
+      <div className="row">
+        <div className="col-md-4 col-lg-4 offset-lg-4">
+          <div className="four-zero-four-bg">
+            <h1 className="title text-danger">404</h1>
+            <p className="subtitle">Oops! Looks like you took a wrong turn.</p>
+            <button className='btn btn-outline-info' onClick={() => navigate(-1)}>Go Back</button>
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
   )
 }
