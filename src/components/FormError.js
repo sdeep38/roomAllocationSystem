43dd366f5ddabe7d2766d38b2aeade856dd91529
Soumyadeep <soumyadip243@gmail.com>
2023-07-01 22:15:33 +0530

    modified component to show icon

--- src/components/FormError.js
+++ src/components/FormError.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 export default function FormError(props) {
     return (
-        <div className={'alert alert-' + (props.status) + ' d-flex align-items-center alert-dismissible fade show'} role="alert">
-            <div>
+        <div className={'alert alert-' + (props?.status) + ' d-flex align-items-center alert-dismissible fade show'} role="alert">
+            <span className='form-error-title'>
+                <i className={"result-mark fa " + (props?.status === 'success' ? 'fa-check' : 'fa-exclamation') + " pe-2"}></i>
                 {props.value}
-            </div>
+            </span>
             {props.dismissable && <span className="fa fa-xmark btn-close" type="button" data-bs-dismiss="alert" aria-label="Close"></span>}
         </div>
     )
